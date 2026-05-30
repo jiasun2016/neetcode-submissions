7a5128c59aa90be43a07ce939830c66b22e0f1f7
Jia Sun <js645967@gmail.com>
2026-05-30 11:31:02 -0700

    Add: swim-in-rising-water - submission-3

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        visited = set()
+        n = len(grid)
+        visited.add((0, 0)) 
+        dirs = [[0,1],[1,0],[-1,0],[0,-1]]  
+        q = [(grid[0][0], 0, 0)]
+       
+        while q:
+            h, x, y = heapq.heappop(q)
+            if x == n-1 and y == n-1:
+                return h 
+            for dx, dy in dirs:
+                nx, ny = dx + x, dy + y 
+                if 0 <= nx < n and 0 <= ny < n and (nx, ny) not in visited:
+                    maxH = max(grid[nx][ny],h) 
+                    visited.add((nx, ny)) 
+                    heapq.heappush(q, (maxH, nx, ny))
+        
+             
